018e3ad430b5cbc2eedbcd05d59e9cae6e7e9c02
hngtiendng <62598734+hngtiendng@users.noreply.github.com>
2021-04-06 16:06:25 +0700

    add Order & OrderDetail Model

---
+++ ServerSite/Models/OrderDetail.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServerSite.Models
+{
+    public class OrderDetail
+    {
+        public int Id { get; set; }
+        public int OrderId { get; set; }
+        public Order Order { get; set; }
+        public double totalPrice { get; set; }
+        public DateTime CraeteDate { get; set; }
+        public Boolean Status { get; set; }
+        public string Address { get; set; }
+        public string UserPhone { get; set; }
+    }
+}

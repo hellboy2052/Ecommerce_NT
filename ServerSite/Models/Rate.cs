a290416c85c6727c9d80f0571c78a041421dbe43
hngtiendng <62598734+hngtiendng@users.noreply.github.com>
2021-04-06 14:50:01 +0700

    add Rate Model

---
+++ ServerSite/Models/Rate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServerSite.Models
+{
+    public class Rate
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public int totalStar { get; set; }
+        public string Comment { get; set; }
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+        public User User { get; set; }
+    }
+}

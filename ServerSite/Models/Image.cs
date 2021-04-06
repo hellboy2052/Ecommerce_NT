dfe525f88f7b5923ce14661e3922a36d495c5fc1
hngtiendng <62598734+hngtiendng@users.noreply.github.com>
2021-04-06 14:52:01 +0700

    add Image Model

---
+++ ServerSite/Models/Image.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServerSite.Models
+{
+    public class Image
+    {
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+        public string ImagePath { get; set; }
+    }
+}

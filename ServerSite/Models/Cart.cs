69fadf46e10fc28d20300f05e6579e89269a5850
hngtiendng <62598734+hngtiendng@users.noreply.github.com>
2021-04-06 14:56:56 +0700

    add Cart Model

---
+++ ServerSite/Models/Cart.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServerSite.Models
+{
+    public class Cart
+    {
+        public string UserId { get; set; }
+        public User User { get; set; }
+        public int Id { get; set; }
+        public ICollection<Product> Products { get; set; }
+    }
+}

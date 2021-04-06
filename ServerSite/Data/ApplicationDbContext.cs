9cbe2b79ef475581dce744f6559e761a56c9060b
hngtiendng <62598734+hngtiendng@users.noreply.github.com>
2021-04-06 15:01:19 +0700

    update DbSet on DB Context

--- ServerSite/Data/ApplicationDbContext.cs
+++ ServerSite/Data/ApplicationDbContext.cs
@@ -1,16 +1,23 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using ServerSite.Models;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace ServerSite.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityUserContext<User>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Image> Images { get; set; }
+        public DbSet<Rate> Rates { get; set; }
+        public DbSet<Cart> Carts { get; set; }
     }
 }

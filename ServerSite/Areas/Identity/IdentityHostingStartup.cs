0383c0b6a21dd5dee13875bdf0d1364ed96c4bff
hngtiendng <62598734+hngtiendng@users.noreply.github.com>
2021-04-23 13:58:26 +0700

    fix redirect logout

---
+++ ServerSite/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ServerSite.Data;
+using ServerSite.Models;
+
+[assembly: HostingStartup(typeof(ServerSite.Areas.Identity.IdentityHostingStartup))]
+namespace ServerSite.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
--- ServerSite/Startup.cs
+++ ServerSite/Startup.cs
@@ -113,6 +113,7 @@ namespace ServerSite
             {
                 app.UseDeveloperExceptionPage();
                 app.UseMigrationsEndPoint();
+                app.UseMigrationsEndPoint();
             }
             else
             {

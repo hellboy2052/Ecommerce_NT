f41f4a92fa70d6a39999f9c52955ae2c5b241f17
hngtiendng <62598734+hngtiendng@users.noreply.github.com>
2021-04-25 23:51:46 +0700

    fix port when run watch server from terminal

--- AdminSite/admin-site/src/api/index.js
+++ AdminSite/admin-site/src/api/index.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-axios.defaults.baseURL = "https://localhost:44309";
+//axios.defaults.baseURL = "https://localhost:44309";
+axios.defaults.baseURL = "https://localhost:5001";
 
 
 const Product = {

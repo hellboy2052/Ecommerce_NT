ee84b93cccef90b4d08ecf1c15157497e1f1f172
hngtiendng <62598734+hngtiendng@users.noreply.github.com>
2021-04-28 12:13:08 +0700

    add descripttion for Cate

---
+++ ServerSite/Migrations/20210428051218_ver1.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ServerSite.Migrations
+{
+    public partial class ver1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Categories",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Categories");
+        }
+    }
+}
--- ServerSite/Models/Category.cs
+++ ServerSite/Models/Category.cs
@@ -7,5 +7,7 @@ namespace ServerSite.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public virtual ICollection<Product> Products { get; set; }
+        public string Description { get; set; }
+
     }
 }
